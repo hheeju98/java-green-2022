3788e00d66ceab996123ca20bb88733b12f63f10
pys <bark6256@naver.com>
2022-01-25 17:16:29 +0900

    공공

--- src/data00/Test01.java
+++ src/data00/Test01.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 import com.google.gson.Gson;
 
---
+++ src/data00/Test02.java
@@ -0,0 +1,37 @@
+package data00;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+
+import java.net.URL;
+import java.net.http.HttpConnectTimeoutException;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import data00.AirportDto.Response.Body.Items.Item;
+
+public class Test02 {
+    public static void main(String[] args) {
+        URL url;
+        try {
+            url = new URL(
+                    "http://openapi.tago.go.kr/openapi/service/DmstcFlightNvgInfoService/getArprtList?serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&_type=json");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), "utf-8"));
+            String responseJson = br.readLine();
+            Gson gson = new Gson();
+            AirportDto dto = gson.fromJson(responseJson, AirportDto.class);
+            List<Item> result = dto.getResponse().getBody().getItems().getItem();
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("주소 입력이 잘못되었습니다.");
+        }
+    }
+}
