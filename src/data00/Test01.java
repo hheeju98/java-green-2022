ff8de54c46011904813f845fba8ea5a4d26c9001
pys <bark6256@naver.com>
2022-01-26 17:53:34 +0900

    1-26

--- src/data00/Test01.java
+++ src/data00/Test01.java
@@ -8,27 +8,33 @@ import java.util.List;
 
 import com.google.gson.Gson;
 
+import data00.ResponseDto.Response.Body.Items.Item;
+
 public class Test01 {
     public static void main(String[] args) {
-        URL url = new URL(" http://openapi.tago.go.kr/openapi/service/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&numOfRows=10&pageNo=1&depAirportId=NAARKPK&arrAirportId=NAARKPC&depPlandTime=20220125&airlineId=ABL&_type=json")
- //conn -> Byte Stream 선!!
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-//원래 default 1Byte 한글을 읽었기 때문에 한글이 깨졌다.
-        //utf-8 한글을 3Byte로 끊어 읽겠다.
-BufferedReader br = new BufferedReader(
-    new InputStreamReader(conn.getInputStream(),"utf-8")
-);
-String responseJson = br.readLine();
-Gson gson = new Gson();
-ResponseDto dto = gson.fromJson(responseJson,ResponseDto.class);
-List<Item> result = dto.getResponse().getBody().getItems().getItem();
-System.out.println(dto);
+        try {
+            URL url = new URL(
+                    "http://openapi.tago.go.kr/openapi/service/DmstcFlightNvgInfoService/getFlightOpratInfoList?serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&numOfRows=10&pageNo=1&depAirportId=NAARKPK&arrAirportId=NAARKPC&depPlandTime=20220125&airlineId=ABL&_type=json");
 
-System.out.println("받은문자열 :" temp);
-    } catch (Exception e){
-        System.out.println("주소입력이 잘못되었습니다.");
+            // conn -> Byte Stream 선!!
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // 원래 default 1Byte 한글을 읽었기 때문에 한글이 깨졌다.
+            // utf-8 한글을 3Byte로 끊어 읽겠다.
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+            String responseJson = br.readLine();
+            Gson gson = new Gson();
+            ResponseDto dto = gson.fromJson(responseJson, ResponseDto.class);
+            List<Item> result = dto.getResponse().getBody().getItems().getItem();
+
+            System.out.println(result);
+            // System.out.println(dto);
+            // System.out.println(dto.getResponse().getBody().getItems().getItem().get(0));
+
+        } catch (Exception e) {
+            System.out.println("주소 입력이 잘못되었습니다.");
+        }
     }
-
-
-    
 }
