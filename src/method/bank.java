5514a3ac406e13a4f931329e47b97a9429d5645a
ssar <ssar@naver.com>
2022-02-17 00:05:55 +0900

    method 교재

--- src/method/bank.java
+++ src/method/bank.java
@@ -1,5 +1,26 @@
 package method;
 
-public class bank {
+public class Bank {
+    int money, interest, withdraw, deposit;
 
+    public void deposit() {
+        deposit = money + interest - withdraw;
+    }
+
+    public int withdraw() {
+        return withdraw;
+    }
+
+    public int money() {
+        return money;
+
+    }
+
+    public void interest() {
+        interest = (int) (money * 0.02);
+    }
+
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+    }
 }
