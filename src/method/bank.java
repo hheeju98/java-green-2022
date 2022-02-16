6e6a58c74c972f2d320e2a060ec3d25e96fecc46
ssar <ssar@naver.com>
2022-02-17 00:11:35 +0900

    method교재

--- src/method/bank.java
+++ src/method/bank.java
@@ -1,23 +1,27 @@
 package method;
 
 public class Bank {
-    int money, interest, withdraw, deposit;
+    int money, interest, withdraw, deposit, putMoney;
 
     public void deposit() {
-        deposit = money + interest - withdraw;
+        deposit = money + interest - withdraw + putMoney;
     }
 
     public int withdraw() {
-        return withdraw;
+        return withdraw; // 인출
     }
 
     public int money() {
-        return money;
+        return money; // 원래계좌돈
 
     }
 
     public void interest() {
-        interest = (int) (money * 0.02);
+        interest = (int) (money * 0.02); // 이자
+    }
+
+    public int putMoney() {
+        return putMoney; // 돈넣기
     }
 
     public static void main(String[] args) {
